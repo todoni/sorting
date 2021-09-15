57e695ea50ae5216743483cdc504b1d06b1c0ada
sohan <sohan@student.42seoul.kr>
2021-09-15 17:29:29 +0900

    feat: Swap just one time

--- selection.c
+++ selection.c
@@ -44,9 +44,37 @@ void	BubbleSort(int arr[], int size)
 	}
 }
 
+void	QuickSort(int arr[], int left, int right)
+{
+	int size = right - left + 1;
+	int	pivot_index = (size / 2) + left;
+	int pivot_value = arr[pivot_index];
+
+	Swap(&arr[pivot_index], &arr[right]);
+	right--;
+	while (left != right)
+	{
+		if (arr[left] >= pivot_value)
+			break;
+		left++;
+	}
+	while (left != right)
+	{
+		if (arr[right] < pivot_value)
+			break;
+		right--;
+	}
+	if (left == right)
+		Swap(&arr[pivot_index], &arr[left]);
+	else
+		Swap(&arr[left], &arr[right]);
+
+
+}
+
 int	main()
 {
-	int	arr[10] = {100, 50, 10, 10, 100, 20, 40, 30, 90, 80};
+	int	arr[10] = {100, 50, 60, 10, 15, 20, 40, 30, 90, 80};
 	int	len = sizeof(arr) / sizeof(int);
 
 	//Swap(&arr[0], &arr[1]);
@@ -55,9 +83,9 @@ int	main()
 		printf("%d ", arr[i]);
 	printf("\n");
 	//SelectionSort(arr, len);
-	BubbleSort(arr, len);
+	//BubbleSort(arr, len);
 	//printf("%d\n", len);
-
+	QuickSort(arr, 0, len - 1);
 	for (int i = 0; i < 10; i++)
 		printf("%d ", arr[i]);
 	printf("\n");
